4dfc5ff27c647738cf7bb2a40fc192f2316c2501
SorcerReyn <ryanshef01@gmail.com>
2021-03-22 23:59:47 -0400

    Style Update
    I figured out the tabbing conventions.

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -5,11 +5,11 @@ import Header from "../components/header"
 export default function Home() {
   return (
     <div style={{ color: 'purple' }}>
-      <Link to="/contact/">Contact</Link>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-      
+    	<Link to="/contact/">Contact</Link>
+    	<h1>Hello Gatsby!</h1>
+    	<p>What a world.</p>
+    	<img src="https://source.unsplash.com/random/400x200" alt="" />
+        
     </div>
   )
 }
