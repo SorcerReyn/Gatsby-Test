5ae23f7a98f7f4972e0dcd70e658cd497f976f8a
SorcerReyn <ryanshef01@gmail.com>
2021-03-29 02:12:50 -0400

    Finished Tutorial 8
    Did tutorial 8, which covered some useful plugins involving metadata and other cool plugins.

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
 		title: `Pandas Eating Lots`,
+		description: `A simple description about pandas eating lots...`,
+		author: `Ryan`,
 	},
 	plugins: [
 		{
@@ -18,5 +20,19 @@ module.exports = {
 				pathToConfigModule: `src/utils/typography`,
 			},
 		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `GatsbyJS`,
+				short_name: `GatsbyJs`,
+				start_url: `/`,
+				background_color: `#6b37bf`,
+				theme_color: `#6b37bf`,
+				display: `standalone`,
+				icon: `src/images/icon.jpg`
+			},
+		},
+		`gatsby-plugin-offline`,
+		`gatsby-plugin-react-helmet`,
 	],
 }
